c4cf7ec742719243893530f62c4060b319f624bc
Jasna Kucevic <jasnakucevic1994@gmail.com>
2021-04-01 21:41:12 +0200

    Napravljena pocetna stranica

--- pet/project.clj
+++ pet/project.clj
@@ -3,4 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [ring/ring-jetty-adapter "1.9.2"]
+                 [ring/ring "1.9.2"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.5.1"]
+                 [ring/ring-core "1.9.2"]
+                 [ring-json-response "0.2.0"]
+                 [compojure "1.4.0"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring { :handler pet.core/foo})
--- pet/src/pet/core.clj
+++ pet/src/pet/core.clj
@@ -1,6 +1,53 @@
-(ns pet.core)
+(ns pet.core
+  (:use [hiccup.element :only (link-to)])
+  (:require [hiccup.page :as h]
+            [hiccup.page :refer [html5 include-css]]
+            [compojure.core :refer [defroutes ANY]]
+            [compojure.route :as route]
+            [compojure.core :refer [defroutes GET POST]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+  )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+ (defn index []
+  (h/html5
+    [:head
+     [:title "PetProtectors"]
+     (include-css "/css/style.css" "/css/RegCss.css" "/css/bootstrap.min.css"
+                  "css/font-awesome.min.css" "/css/animate.min.css"
+                  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")
+     ]
+    [:body {:class "homepage"}
+      [:header {:id "header"}
+    [:div {:class "container"}
+
+      [:div {:id "logo" :class "pull-left"}
+        [:a {:href "#hero"} [:img {:src "" :alt "" :title "" }]]
+       
+        [:h3 [:a {:href  "#hero"}"PetProtectors"]]
+        ]
+
+      [:nav {:id "nav-menu-container"}
+        [:ul {:class "nav-menu"}
+          [:li {:class ""}[:a {:href "#hero"} "Početna strana"]]
+          
+        ]
+      ]
+    ]
+  ]
+   [:section {:id "hero"}
+     [:div {:class "hero-container"}
+       [:h3 "Dobrodošli na portal pomoći vašim izgubljenim ljubimcima"]
+       [:h4 "Tu smo da povratimo vaše voljene drugare"]
+       [:a {:href "#about" :class "btn-get-started"} "Upoznajte se sa nama"]
+     ]
+   ]]
+   )
+  )
+
+(defroutes routes
+ (GET "/" [] (index))
+ (route/resources "/"))
+
+(def foo
+  (wrap-defaults routes site-defaults))
+
